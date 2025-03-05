1d4da0a281682202a634f04b39eb264e2271744b
onelrian <onelsob57@gmail.com>
2025-03-05 11:07:38 +0100

    CI: Implemented the execute method

--- src/threadpool.rs
+++ src/threadpool.rs
@@ -1,4 +1,4 @@
-use std::{sync::mpsc, thread};
+use std::{sync::{mpsc, Arc, Mutex}, thread};
 
 use crate::{job::Job, worker::Worker};
 
@@ -15,25 +15,27 @@ impl ThreadPool {
     ///
     /// # Panics
     ///
-    /// The `new` function will panic if the size is zero.
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
 
         let (sender, receiver) = mpsc::channel();
 
+        let receiver = Arc::new(Mutex::new(receiver));
+
         let mut workers = Vec::with_capacity(size);
 
         for id in 0..size {
-            workers.push(Worker::new(id));
+            workers.push(Worker::new(id, Arc::clone(&receiver)));
         }
 
         ThreadPool { workers, sender }
     }
-    // --snip--
 
     pub fn execute<F>(&self, f: F)
     where
         F: FnOnce() + Send + 'static,
     {
+        let job = Box::new(f);
+        self .sender.send(job).unwrap()
     }
 }
