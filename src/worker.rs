a512fed2a8a9bb146c9cb7a3c8b34540a4b84197
onelrian <onelsob57@gmail.com>
2025-03-05 11:07:02 +0100

    CI: used a smart pointer for the channel receiver

--- src/worker.rs
+++ src/worker.rs
@@ -1,17 +1,18 @@
-use std::thread;
+use std::{sync::{mpsc, Arc, Mutex}, thread};
+
+use crate::job::Job;
 
 pub struct Worker {
     id: usize,
-    thread : thread::JoinHandle<()>,
+    thread: thread::JoinHandle<()>,
 }
 
 impl Worker {
-    pub fn new(id: usize) -> Self {
-        let thread = thread::spawn(|| {});
+    pub fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Self {
+        let thread = thread::spawn(|| {
+            receiver;
+        });
 
-        Worker{
-            id,
-            thread
-        }
+        Worker { id, thread }
     }
 }
